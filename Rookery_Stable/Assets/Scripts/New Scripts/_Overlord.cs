482c5ebea338dc91daadbf065b113cfe5fe8b77f
K Norman <k.a.norman@live.com>
2013-05-15 16:15:57 -0700

    Fireball feature implementation
    Added features to fireball, including gold cost, cooldown timer, and
    increasing cost each time it is used.  All of these values can be
    adjusted during balancing.  Currently the number of ticks until the
    fireball is ready again is displayed in the corner of the screen.  This
    can later be replaced with some other indicator if we eventually have
    assets to devote to it.

--- Rookery_Stable/Assets/Scripts/New Scripts/_Overlord.cs
+++ Rookery_Stable/Assets/Scripts/New Scripts/_Overlord.cs
@@ -19,12 +19,16 @@ public class _Overlord : MonoBehaviour {
     //Global Variables
     public static int 	gold, wyrmHealth, waveNumber, worldTime,
 						egg_cost, fire_cost, ice_cost, lightening_cost,
-						egg_sale, fire_sale, ice_sale, lightening_sale;
+						egg_sale, fire_sale, ice_sale, lightening_sale,
+						fireball_base_cost, fireball_current_cost, 
+						fireball_cooldown, fireball_timer;
+	public static double fireball_increase;
 
 	// Initialization
-	void Start () {
+	void Start ()
+	{
         wyrmHealth = 100;
-        gold = 200;
+        gold = 300;
         waveNumber = 1;
         worldTime = 1;
 		
@@ -37,10 +41,20 @@ public class _Overlord : MonoBehaviour {
 		ice_sale = 125;
 		lightening_cost = 50;
 		lightening_sale = 125;
+		
+		// Fireball values
+		fireball_base_cost = 100;
+		fireball_current_cost = fireball_base_cost;
+		fireball_cooldown = 400;
+		fireball_timer = 0;
+		fireball_increase = 1.1;
 	}
 	
 	// Called once per frame, do not overwhelm
-	void Update () {
-	
+	void Update ()
+	{
+		// Decrement the fireball cooldown timer if not ready yet
+		if (fireball_timer > 0)
+			fireball_timer--;
 	}
 }
